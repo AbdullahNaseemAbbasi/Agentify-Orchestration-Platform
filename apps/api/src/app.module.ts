01af35227d7978b370cb6ba1151f0b631ee2e2ed
AbdullahNaseemAbbasi <abdullahnaseem1386@gmail.com>
2026-04-29 01:02:25 +0500

    add UsersModule and wire into AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from '@agentify/database';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [DatabaseModule, HealthModule],
+  imports: [DatabaseModule, HealthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
