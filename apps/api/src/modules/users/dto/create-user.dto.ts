b8181c7d3a307e3218b6267e4b5c8eb223830cfb
AbdullahNaseemAbbasi <abdullahnaseem1386@gmail.com>
2026-04-29 01:01:30 +0500

    add CreateUserDto with class-validator constraints

---
+++ apps/api/src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail({}, { message: 'email must be a valid email address' })
+  @MaxLength(255)
+  email!: string;
+
+  @IsString()
+  @MinLength(8, { message: 'password must be at least 8 characters' })
+  @MaxLength(128)
+  password!: string;
+
+  @IsString()
+  @MinLength(1, { message: 'name cannot be empty' })
+  @MaxLength(100)
+  name!: string;
+}
